a63f49d079acea9dc7f3839a0832e245aaeee5a0
Abel Kalum <chumbabel@gmail.com>
2020-07-12 23:01:10 +0000

    modify reviews and users controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,44 +1,39 @@
 class ReviewsController < ApplicationController
  
-#CREATE
-    get '/reviews/new' do 
-       erb :'/reviews/new'
-    end 
-    post '/reviews' do 
-       @review = Review.create(title: params[:title], author: params[:author], review: params[:review])
-       redirect "/reviews/#{@review.id}"
-    end
-#READ
-    get '/reviews/:id' do 
-       @review = Review.find_by_id(params[:id])
-      
-       erb :'reviews/show'
-    end 
- 
-    get '/reviews' do 
-       @reviews = Review.all 
-            
-       erb :'/reviews/index'
-      end 
-     end
-#UPDATE   
-     get '/reviews/:id/edit' do 
-         @review = Review.find_by_id(params[:id])
-          erb :'/reviews/edit'
-      end
- 
-      patch '/reviews/:id' do 
-          @review = Review.find_by_id(params[:id])
-          @review.text = params[:new_review]
-          @review.save
-          redirect to "/reviews/#{@review.id}"
-        end 
-      end
-#DELETE
-      delete '/reviews/:id' do 
-          @review = Review.find_by_id(params[:id])
-          @review.delete
-          redirect '/users/show'
-        end
-     end
+  get '/reviews/new' do 
+    erb :'/reviews/new'
+  end 
+    
+  post '/reviews' do 
+    @review = Review.create(title: params[:title], author: params[:author], review: params[:review])
+    redirect "/reviews/#{@review.id}"
   end
+
+  get '/reviews/:id' do 
+    @review = Review.find_by_id(params[:id])
+    erb :'reviews/show'
+  end 
+ 
+  get '/reviews' do 
+    @reviews = Review.all 
+    erb :'/reviews/index'
+  end
+
+  get '/reviews/:id/edit' do 
+    @review = Review.find_by_id(params[:id])
+    erb :'/reviews/edit'
+  end
+ 
+  patch '/reviews/:id' do 
+    @review = Review.find_by_id(params[:id])
+    @review.text = params[:new_review]
+    @review.save
+    redirect to "/reviews/#{@review.id}"
+  end
+
+  delete '/reviews/:id' do 
+    @review = Review.find_by_id(params[:id])
+    @review.delete
+    redirect '/users/show'
+  end
+end
