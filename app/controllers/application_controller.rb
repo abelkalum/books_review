e0c5f8dcfbabb851a0a8f9c9433032f17e955dfc
Abel Kalum <chumbabel@gmail.com>
2020-07-12 21:47:23 +0000

    update app controller and create user ontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,32 @@
-require './config/environment'
-
-class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get "/" do
-    erb :welcome #change
-  end
-
+require "./config/environment"
+require "./app/models/user"
+require "./app/models/book"
+require "./app/models/review"
+ 
+class ApplicationController < Sinatra::Base 
+ 
+    configure do 
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "fastly"
+    end
+ 
+    get '/' do
+      erb :"/application/home"
+    end
+ 
+    helpers do 
+      def logged_in?
+        !!session[:user_id]
+      end
+ 
+      def current_user
+        User.find(session[:user_id])
+      end
+ 
+      def authorized?(review)
+        current_user.id == review.user_id
+      end
+   end
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController 
+   get '/users/signup' do
+      erb :'/users/signup'
+   end
+ 
+   post '/users/signup'
+      if params[:email] == “” && params[:password] == “”
+        redirect “/users/signup”
+      else
+        @user = User.create(email: params[:email], password: params[:password])
+        sessions[:user_id] = @user.id
+        redirect “/users/#{@user.id}”
+       end
+     end
+ 
+    get '/users/login' do 
+      if !logged_in?
+        erb :'/users/login'
+      else
+        @user = User.find_by(session[:id])
+        redirect “/users/#{@user.id}”
+      end
+     end
+
+    post '/users/login' do 
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+           session[:user_id] = @user.id
+           redirect “/users/#{@user.id}”
+        else 
+          redirect “/users/login”
+        end
+    end
+
+    get '/users/:id' do
+         @user = User.find(params[:id])
+         erb :'/users/show'
+      end
+    get '/logout' do 
+        if logged_in? 
+          session.clear 
+          redirect '/'
+        end
+    end
+ end
